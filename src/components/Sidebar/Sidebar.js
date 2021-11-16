24a8facc0742cfc426132a0c97567c9bb1a929ca
subhankhalid402 <subhan.khalid402@gmail.com>
2021-11-16 20:23:43 +0500

    sidebar updated

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -71,19 +71,24 @@ const Sidebar = (props) => {
   // creates the links that appear in the left menu / Sidebar
   const createLinks = (routes) => {
     return routes.map((prop, key) => {
-      return (
-        <NavItem key={key}>
-          <NavLink
-            to={prop.layout + prop.path}
-            tag={NavLinkRRD}
-            onClick={closeCollapse}
-            activeClassName="active"
-          >
-            <i className={prop.icon} />
-            {prop.name}
-          </NavLink>
-        </NavItem>
-      );
+      if (prop.layout === "/admin") {
+        return (
+            <NavItem key={key}>
+              <NavLink
+                  to={prop.layout + prop.path}
+                  tag={NavLinkRRD}
+                  onClick={closeCollapse}
+                  activeClassName="active"
+              >
+                <i className={prop.icon}/>
+                {prop.name}
+              </NavLink>
+            </NavItem>
+        );
+      }
+      else{
+        return null
+      }
     });
   };
 
